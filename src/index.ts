a8af830d1a112f0b48059d5c29210061f7e60f47
Sónia Reis <soniapdosreis@gmail.com>
2025-04-19 19:18:36 +0100

    code cleanup

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,16 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
 
-app.post('/', async (req, res) => {
+app.post('/webhook', async (req, res) => {
   const event = req.body;
 
+  console.log(event)
+
   if (event.event_type === 'LIBRARY_PUBLISH') {
-    const { file_key, file_name, description, created_at, created_by } = event;
+    const { file_key, file_name, description, timestamp, triggered_by } = event;
 
     const slackMessage = {
-      text: `📚 *Figma Library Published*\n*File Name:* ${file_name}\n*Published by:* ${created_by?.name || 'Unknown'}\n*Time:* ${new Date(created_at).toLocaleString()}\n*Description:* ${description || 'No description'}`
+      text: `📚 *Figma Library Published*\n*File Name:* ${file_name}\n*Published by:* ${triggered_by?.handle || 'Unknown'}\n*Time:* ${new Date(timestamp).toLocaleString()}\n*Description:* ${description || 'No description'}`
     };
 
     try {
@@ -25,10 +27,12 @@ app.post('/', async (req, res) => {
       console.log('Posted to Slack');
     } catch (err) {
       console.error('Slack error:', err);
+      return res.status(500).send(JSON.stringify({message: "unexpected error: ${err}"}))
     }
+    return res.status(200).send('OK');
+  } else {
+    return res.status(200).send(JSON.stringify({message: "nothing to do"}))
   }
-
-  res.status(200).send('OK');
 });
 
 app.listen(PORT, () => {
