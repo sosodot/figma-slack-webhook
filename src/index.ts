bd8e25408e84bf487300a98b287bbee509026f19
Sónia Reis <soniapdosreis@gmail.com>
2025-04-19 22:04:08 +0100

    added support for figma passcode

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 const SLACK_WEBHOOK_URL = process.env.SLACK_WEBHOOK_URL;
+const FIGMA_PASSCODE = process.env.FIGMA_PASSCODE || "";
 
 app.get('/', async (req, res) => {
   return res.status(200).send(JSON.stringify({message: "these are not the unicorns you're looking for"}))
@@ -17,7 +18,11 @@ app.get('/', async (req, res) => {
 app.post('/webhook', async (req, res) => {
   const event = req.body;
 
-  console.log(event)
+  if (FIGMA_PASSCODE != "") {
+    if (event.passcode != FIGMA_PASSCODE){
+      return res.status(401).send(JSON.stringify({message: "401 Unauthorized"}))
+    }
+  }
 
   if (event.event_type === 'LIBRARY_PUBLISH') {
     const { file_key, file_name, description, timestamp, triggered_by } = event;
